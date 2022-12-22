76e28502a3662b94f449d5237bb9076aed1814e3
Divya <beingdivyadixit@gmail.com>
2022-12-22 18:27:36 +0530

    Leap Year Program

--- BasicCoreProgramsDay5/Program.cs
+++ BasicCoreProgramsDay5/Program.cs
@@ -6,9 +6,27 @@ namespace BasicCoreProgramsDay5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Number of times you wanted to flip the coin");
-            int N= Convert.ToInt32(Console.ReadLine());
-            flipCoin.coinFlip(N);
+            Console.WriteLine(" Press 1 for Flip Coin and print percentage of Heads and Tails Program \n Press 2 for Leap Year Program");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                     Console.WriteLine("Enter the Number of times you wanted to flip the coin");
+                     int N = Convert.ToInt32(Console.ReadLine());
+                     flipCoin.coinFlip(N);
+                    break;
+                case 2:
+                     Console.WriteLine("Enter the year to check it's leap year or not");
+                     int Year=Convert.ToInt32(Console.ReadLine());
+                    if(Year.ToString().Length ==4)
+                       leapYear.yearLeap(Year);
+                    else
+                        Console.WriteLine("Invalid Input");
+                    break;
+                default:
+                    Console.WriteLine("Please Select Correct input");
+                    break;
+            }
         }
     }
 }
---
+++ BasicCoreProgramsDay5/leapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay5
+{
+    internal class leapYear
+    {
+        public static void yearLeap(int Year)
+        {  //if & else if statement for checking year is leap or not
+            if (Year%400==0) 
+            {
+                Console.WriteLine("it's Leap Year"); 
+            }
+            else if(Year%100==0)
+            {
+                Console.WriteLine("it's not a Leap Year");
+            }
+            else if(Year%4==0) { }
+            {
+                Console.WriteLine("it's Leap Year");
+            }
+        }
+    }
+}
