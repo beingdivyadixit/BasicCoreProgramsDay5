352b9001d15a33fc6cdfdd9b5f9a974004f9c1b5
Divya <beingdivyadixit@gmail.com>
2022-12-22 18:07:20 +0530

    Flip Coin and print percentage of Heads and Tails Program

--- BasicCoreProgramsDay5/Program.cs
+++ BasicCoreProgramsDay5/Program.cs
@@ -7,8 +7,8 @@ namespace BasicCoreProgramsDay5
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the Number of times you wanted to flip the coin");
-            int N= Convert.ToInt32(Console.ReadLine());
-            flipCoin.coinFlip(N);
+            int N= Convert.ToInt32(Console.ReadLine()); // stored user input by converting data typ into int
+            flipCoin.coinFlip(N);  //class.method
         }
     }
 }
--- BasicCoreProgramsDay5/flipCoin.cs
+++ BasicCoreProgramsDay5/flipCoin.cs
@@ -9,25 +9,26 @@ namespace BasicCoreProgramsDay5
     internal class flipCoin
     {
         public static void coinFlip(int N)
-        {
+        {   //variables
             int TailsCount = 0, HeadCount = 0;
             Random random = new Random();
+            //loop for flipping coin N (input by user) times 
             for(int i=0; i<N; i++)
             {
                 double FlipCoin = random.NextDouble();
-                if(FlipCoin<0.5)
+                if(FlipCoin<0.5)   // if-else statement for checking it's head or tails ... Tails if FlipCoin<0.5  else Head
                 {
-                    TailsCount = TailsCount + 1;
+                    TailsCount = TailsCount + 1;  // counting Tails count
                     Console.WriteLine("It's Tails");
                 }
                 else
                 {
-                    HeadCount = HeadCount + 1;
+                    HeadCount = HeadCount + 1; // counting head count
                     Console.WriteLine("It's Head");
                 }
             }
-            double PercentageOfHead = (HeadCount * 100)/N;
-            double PercentageOfTails = (TailsCount * 100)/N;
+            double PercentageOfHead = (HeadCount * 100)/N; // calculating percentage of head comming
+            double PercentageOfTails = (TailsCount * 100)/N; // calculating percentage of tails comming
             Console.WriteLine("Head came : {0} , Tails came : {1} , Percentage of Head : {2}% & Percentage of Tails : {3}% ",HeadCount,TailsCount,PercentageOfHead,PercentageOfTails);
         }
     }
