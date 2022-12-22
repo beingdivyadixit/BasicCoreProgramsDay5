fd132bc07083cfe0ebf4bd334cd070def02a0363
Divya <beingdivyadixit@gmail.com>
2022-12-22 17:58:36 +0530

    Flip Coin and print percentage of Heads and Tails Program

--- BasicCoreProgramsDay5/Program.cs
+++ BasicCoreProgramsDay5/Program.cs
@@ -6,7 +6,9 @@ namespace BasicCoreProgramsDay5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Basic Core Programs Day 5");
+            Console.WriteLine("Enter the Number of times you wanted to flip the coin");
+            int N= Convert.ToInt32(Console.ReadLine());
+            flipCoin.coinFlip(N);
         }
     }
 }
---
+++ BasicCoreProgramsDay5/flipCoin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay5
+{
+    internal class flipCoin
+    {
+        public static void coinFlip(int N)
+        {
+            int TailsCount = 0, HeadCount = 0;
+            Random random = new Random();
+            for(int i=0; i<N; i++)
+            {
+                double FlipCoin = random.NextDouble();
+                if(FlipCoin<0.5)
+                {
+                    TailsCount = TailsCount + 1;
+                    Console.WriteLine("It's Tails");
+                }
+                else
+                {
+                    HeadCount = HeadCount + 1;
+                    Console.WriteLine("It's Head");
+                }
+            }
+            double PercentageOfHead = (HeadCount * 100)/N;
+            double PercentageOfTails = (TailsCount * 100)/N;
+            Console.WriteLine("Head came : {0} , Tails came : {1} , Percentage of Head : {2}% & Percentage of Tails : {3}% ",HeadCount,TailsCount,PercentageOfHead,PercentageOfTails);
+        }
+    }
+}
