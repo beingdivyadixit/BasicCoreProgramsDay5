e1e5d288382887d05399a31ce63f7120089bdbbf
Divya <beingdivyadixit@gmail.com>
2022-12-22 17:36:22 +0530

    Welcome To Basic Core Programs Day 5

---
+++ BasicCoreProgramsDay5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCoreProgramsDay5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Basic Core Programs Day 5");
+        }
+    }
+}
